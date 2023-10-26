46c9c34f3447f9ebf576bdb25db2fbc80664546f
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-26 15:53:29 -0400

    server starts, 6 mvp tests are currently passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// require your server and launch it
+// require your server and launc
+
+const server = require('./api/server')
+const port = 9000;
+
+
+
+// Start the server on the specified port
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
